26ef28f7f9c571afb6f844e853561d7eab1b119a
Ssalifab <ssalilwanga7@gmail.com>
2025-07-18 14:36:09 +0300

    Farmer dashboard GUI started

--- public/scripts/salesDashboard.js
+++ public/scripts/salesDashboard.js
@@ -1,47 +1,53 @@
-const form = document.getElementById('addStockForm');
+const form = document.getElementById('newRequestForm');
 
 form.addEventListener('submit', function (event) {
 event.preventDefault()
 
-const chickType = document.getElementById('chickType').value.trim();
-const breed = document.getElementById('breed').value.trim();
+const farmerName = document.getElementById('farmerName').value.trim();
+const nin = document.getElementById('nin').value.trim();
+const type = document.getElementById('type').value;
+const breed = document.getElementById('breed').value;
+const farmerType = document.getElementById('farmerType').value;
 const quantity = document.getElementById('quantity').value;
-const age = document.getElementById('age').value;
 const dateUpdated = document.getElementById('dateUpdated').value.trim();
 const errorMsg = document.getElementById('errorMsg');
 errorMsg.textContent = '';
 
 //Form Validation
-    if (!chickType || !breed || !quantity || !age || !dateUpdated) {
+    if (!farmerName || !nin || !type || !breed || !farmerType || !quantity || !dateUpdated) {
         errorMsg.textContent = 'Please fill all fields';
         return;
     }
-        if (quantity < 0 || age < 0) {
-        errorMsg.textContent = 'Age or Quantity must be greater than zero';
+        if (quantity < 0) {
+        errorMsg.textContent = 'Quantity must be greater than zero';
         return;
     }
 
-    addToStockTable(chickType, breed, quantity, age, dateUpdated);
+    addToRequestsTable(farmerName, nin, type, breed, farmerType, quantity, dateUpdated);
     form.reset();
 
 });
 // //table handling
-function addToStockTable(chickType, breed, quantity, age, dateUpdated) {
-    const table = document.getElementById('stockTable').getElementsByTagName('tbody')[0];
+function addToRequestsTable(farmerName, nin, type, breed, farmerType, quantity, dateUpdated) {
+    const table = document.getElementById('requestsTable').getElementsByTagName('tbody')[0];
     let row = table.insertRow();
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
     let cell5 = row.insertCell(4);
+    let cell6 = row.insertCell(5);
+    let cell7 = row.insertCell(6);
     
     // const date = new Date();
     // const formattedDate = date.toLocaleDateString('en-GB');
     // const price = 'Ugx 1,650';
 
-    cell1.textContent= chickType;
-    cell2.textContent=breed;
-    cell3.textContent=quantity;
-    cell4.textContent=age;
-    cell5.textContent=dateUpdated;
+    cell1.textContent= farmerName;
+    cell2.textContent=nin;
+    cell3.textContent=type;
+    cell4.textContent=breed;
+    cell5.textContent=farmerType;
+    cell6.textContent=quantity;
+    cell7.textContent=dateUpdated;
 }
