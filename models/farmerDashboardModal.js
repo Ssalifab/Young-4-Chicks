0b7e0485b185cb7b5fa90a2a1a77cce15ebbd06d
Ssalifab <ssalilwanga7@gmail.com>
2025-08-03 12:06:38 +0300

    Authentication on farmer dashboard

--- models/farmerDashboardModal.js
+++ models/farmerDashboardModal.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.Object,
+        ref: "user",
+        required: true,
+    },
+    farmerType: {
+        type: String,
+        required: true,
+        enum: ["starter", "returning"]
+    },
     chickType: {
         type: String,
         required: true
@@ -13,9 +23,23 @@ const requestSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+     unitPrice: {
+        type: Number,
+        required: true
+    },
+     totalPrice: {
+        type: Number,
+        required: true
+    },
     dateUpdated: {
         type: Date,
         required: true
-    }
+    },
+    status: {
+        type: String,
+        enum: ["pending", "approved","dispatched","canceled"],
+        default: "pending"
+    },
+    lastTakenDate: Date
 });
 module.exports = mongoose.model("request", requestSchema);
--- routes/farmerRoutes.js
+++ routes/farmerRoutes.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const request = require("../models/farmerDashboardModal");
+const {ensureAuthenticated, ensureFarmer} = require("../middleware/authMiddleware")
 
 //Farmer dashboard get route
-router.get('/farmer', (req, res) => {
-    res.render('farmerDashboard');
+router.get('/farmer', ensureAuthenticated, ensureFarmer, async(req, res) => {
+    try {
+        const requests = await request.find({user:req.session.user._id});
+        const isStarter = requests.length === 0;
+        res.render('farmerDashboard', {isStarter})
+    } catch (error) {
+        console.error(error.message);
+        res.redirect('/farmer')
+    }
 });
 
 router.post('/farmer', async (req, res) => {
     try {
         console.log(req.body);  //req.body symbolizes everything you are picking from the form
-        const newRequest = new request(req.body);
+        const {farmerType, chickType, breed, quantity, unitPrice, totalPrice, dateUpdated} = req.body;
+        const userId = req.session.user._id;
+        const newRequest = new request({
+            farmerType, chickType, breed, quantity, unitPrice, totalPrice, dateUpdated,
+            user: userId
+        })
         await newRequest.save();
     } catch (error) {
         console.error(error);
         res.status(400).render('farmerDashboard'); //pass the pug file as a parameter
     }
+
+   
+
+
 
 });
 
